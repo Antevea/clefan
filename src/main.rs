832c6ea76a0ca485112b3a83286d1eddd4ea9989
Antevea <Tosvering@gmail.com>
2024-04-12 14:45:17 +0500

    Add argument to set manual fan speed

--- src/main.rs
+++ src/main.rs
@@ -13,29 +13,35 @@ fn main() {
         return;
     }
 
-    let parsed_arg = parse_args(args);
-    match parsed_arg {
-        EParsedArgs::Help => print_help(),
-        EParsedArgs::Duty(duty) => {
-            let res = fan_control::set_fan_speed(duty);
-            match res {
-                Err(err) => eprintln!("Error: {}", err),
-                Ok(()) => println!("Info: Set fan speed to {}%", duty),
+    if let Ok(parsed_arg) = parse_args(args) {
+        match parsed_arg {
+            EParsedArgs::Help => print_help(),
+            EParsedArgs::Duty(duty) => {
+                let res = fan_control::set_fan_speed(duty);
+                match res {
+                    Err(err) => eprintln!("ERROR: {}", err),
+                    Ok(()) => println!("INFO: Set fan speed to {}%", duty),
+                }
             }
-        }
-    };
+        };
+    } else {
+        eprintln!("ERROR: Wrong arguments!\n");
+        print_help();
+    }
 }
 
-fn parse_args(args: Vec<String>) -> EParsedArgs {
+fn parse_args(args: Vec<String>) -> Result<EParsedArgs, ()> {
     if args[1].contains("-h") {
-        return EParsedArgs::Help;
-    } else {
-        return EParsedArgs::Duty(
-            args[1]
+        return Ok(EParsedArgs::Help);
+    } else if args[1].contains("-d") {
+        return Ok(EParsedArgs::Duty(
+            args[2]
                 .trim()
                 .parse::<u8>()
-                .expect(&format!("Error: wrong argument: {}", args[1])),
-        );
+                .expect(&format!("ERROR: Wrong duty percentage {}", args[2])),
+        ));
+    } else {
+        return Err(())
     }
 }
 
@@ -44,6 +50,7 @@ fn print_help() {
     println!("Usage: clefan [fan_duty_percentage]");
     println!("Arguments:\n\t[fan_duty_percentage]\tTarget fan duty - from 0 up to 100");
     println!("\t-h\t\t\tPrint this help and exit");
+    println!("\t-d\t\t\tSet fan duty percentage manually");
     println!("To use without sudo:");
     println!("\tsudo chown root [path/to/clefan]");
     println!("\tsudo chmod u+s [path/to/clefan]");
