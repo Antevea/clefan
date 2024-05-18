b7bd6e01cca0dfc0b3dc06aeee30ffad812200ed
Antevea <Tosvering@gmail.com>
2024-05-19 01:06:15 +0500

    Arguments parser refactoring.

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,62 @@
 use std::env;
 mod fan_control;
 
-enum EParsedArgs {
-    Help,
-    Duty(u8),
-    Temperature,
-}
-
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() <= 1 {
-        println!("Usage: {} [fan_duty_percentage]", args[0]);
-        return;
-    }
+    let cmd_arg = args[0].as_str();
 
-    if let Ok(parsed_arg) = parse_args(args) {
-        match parsed_arg {
-            EParsedArgs::Help => print_help(),
-            EParsedArgs::Duty(duty) => {
-                let res = fan_control::set_fan_speed(duty);
-                match res {
-                    Err(err) => eprintln!("ERROR: {}", err),
-                    Ok(()) => println!("INFO: Set fan speed to {}%", duty),
+    match args.len() {
+        1 => print_help(cmd_arg),
+        2 => {
+            let first = args[1].as_str();
+
+            match first {
+                "-h" => print_help(cmd_arg),
+                "-t" => {
+                    let res = fan_control::get_cpu_temp();
+                    match res {
+                        Err(err) => eprintln!("{}", err),
+                        Ok(cpu_temp) => println!("INFO: CPU temp is {}°C", cpu_temp),
+                    }
+                }
+                _ => {
+                    println!("ERROR: Unexpected argument {}", first);
+                    print_help(cmd_arg);
                 }
             }
-            EParsedArgs::Temperature => {
-                let res = fan_control::get_cpu_temp();
-                match res {
-                    Err(err) => eprintln!("{}", err),
-                    Ok(cpu_temp) => println!("INFO: CPU temp is {}°C", cpu_temp),
+        },
+        3 => {
+            let first = args[1].as_str();
+
+            match first {
+                "-d" => {
+                    let duty = args[2]
+                        .trim()
+                        .parse::<u8>()
+                        .expect(&format!("ERROR: Wrong duty percentage {}", args[2]));
+
+                    let res = fan_control::set_fan_speed(duty);
+                    match res {
+                        Err(err) => eprintln!("ERROR: {}", err),
+                        Ok(()) => println!("INFO: Set fan speed to {}%", duty),
+                    }
+                }
+                _ => {
+                    println!("ERROR: Unexpected argument {}", first);
+                    print_help(cmd_arg);
                 }
             }
-        };
-    } else {
-        eprintln!("ERROR: Wrong arguments!\n");
-        print_help();
+        },
+        _ => {
+            println!("ERROR: Too many arguments");
+            print_help(cmd_arg);
+        }
     }
 }
 
-fn parse_args(args: Vec<String>) -> Result<EParsedArgs, ()> {
-    if args[1].contains("-h") {
-        return Ok(EParsedArgs::Help);
-    } else if args[1].contains("-d") {
-        return Ok(EParsedArgs::Duty(
-            args[2]
-                .trim()
-                .parse::<u8>()
-                .expect(&format!("ERROR: Wrong duty percentage {}", args[2])),
-        ));
-    } else if args[1].contains("-t") {
-        return Ok(EParsedArgs::Temperature);
-    } else {
-        return Err(())
-    }
-}
-
-fn print_help() {
+fn print_help(cmd: &str) {
     println!("Clefan is a fan control utility for Clevo laptops.\n");
-    println!("Usage: clefan -d [fan_duty_percentage]");
+    println!("Usage: {} -d [fan_duty_percentage]", cmd);
     println!("Arguments:");
     println!("\t-h\t\t\tPrint this help and exit");
     println!("\t-d [percentage]\t\tSet fan duty percentage manually: from 0 up to 100");
